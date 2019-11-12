0832dd471e8922049c7fa6394c1ee46df5e0ad1b
AmyntasSynth <michelnuhn@gmail.com>
2019-11-12 12:56:08 +0100

    First commit

--- Amyntas' YT Downloader/Form1.Designer.cs
+++ Amyntas' YT Downloader/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDownloadAudio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(43, 12);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(157, 20);
+            this.txtURL.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL:";
+            // 
+            // btnDownloadAudio
+            // 
+            this.btnDownloadAudio.Location = new System.Drawing.Point(12, 38);
+            this.btnDownloadAudio.Name = "btnDownloadAudio";
+            this.btnDownloadAudio.Size = new System.Drawing.Size(103, 23);
+            this.btnDownloadAudio.TabIndex = 3;
+            this.btnDownloadAudio.Text = "Download Audio";
+            this.btnDownloadAudio.UseVisualStyleBackColor = true;
+            this.btnDownloadAudio.Click += new System.EventHandler(this.btnPlayAudio_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(771, 374);
+            this.Controls.Add(this.btnDownloadAudio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtURL);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Amyntas\' YT-Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDownloadAudio;
     }
 }
 
--- Amyntas' YT Downloader/Form1.cs
+++ Amyntas' YT Downloader/Form1.cs
@@ -12,9 +12,35 @@ namespace Amyntas__YT_Downloader
 {
     public partial class Form1 : Form
     {
+        //VARIABLES
+        
+
+
+        //MAIN FUNCTION
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        //CUSTOM FUNCTIONS
+
+
+
+        //EVENT FUNCTIONS
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private async void btnPlayAudio_ClickAsync(object sender, EventArgs e)
+        {
+            var url = txtURL.Text;
+            var ID = YoutubeExplode.YoutubeClient.ParseVideoId(url);
+
+            var client = new YoutubeExplode.YoutubeClient();
+            var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(ID);
+            var streamInfo = streamInfoSet.Audio.First();
+            await client.DownloadMediaStreamAsync(streamInfo, $"downloaded_video.{"mp3"}");
         }
     }
 }
